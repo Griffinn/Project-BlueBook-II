5f56cc0ad00f5e77b3b05e3bc027e2210a3970cb
Khushi Prasad <khuprajiv@email.com>
2026-04-20 22:51:24 +0530

    Initial upload for Project-BlueBook II

---
+++ README.md
@@ -0,0 +1,2 @@
+# From-the-Other-Side
+Ghost sightings
---
+++ data/stories.js
@@ -0,0 +1,12 @@
+export const stories = [
+    'BBC: Pilot reports "Tic-Tac" shaped craft over London Heathrow',
+    'CNN: Pentagon declassifies 4K footage of the Mojave Desert incident',
+    'Al Jazeera: Glowing orbs witnessed by thousands in rural Oman',
+    'The Guardian: Discovery of "non-human" wreckage on North Sea floor',
+    'France24: Paris power grid fails as silver disk hovers over Eiffel Tower',
+    'NPR: Why a small Nevada town is bracing for a "Mothership" arrival',
+    'Reuters: Supersonic objects tracked by radar over the Mid-Atlantic',
+    'EFP News: Deep-space signal from Proxima Centauri repeats for 3rd day',
+    'ABC News: Navy personnel testify on "trans-medium" craft in the Pacific',
+    'Vice: Viral TikTok claims to show "liquid metal" at a forest landing site'
+];
